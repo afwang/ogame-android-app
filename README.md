883569262bf1277acc68310fe3498df9db190d88
Alexander Wang <afwang@lolcomp.wikaba.com>
2014-09-27 23:50:03 -0700

    Added cookie code to be able to log in. All the major pieces should be there, but we still do not get the correct HTTP responses. Requires more debugging

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ ogame-android-app
 =================
 
 Android application for the Ogame web browser game.
+
+To do list and notes
+====================
+
+- When a client visits http://ogame.org/, there are a bunch of cookies set in a series of redirections. Thus, we can not automatically follow redirections or else the cookie data is lost (partially no thanks to Java having a bugged implementation of HttpCookies.parse()). For every HTTP response, we must extract any Set-cookie headers and save those cookies. The first cookie that is set is an SID cookie (probably stands for "Session ID"). We then follow a redirection, and then another HTTP response will contain a "deviceID" cookie. The key is that we do set the "follow redirection" property in HttpUrlConnection to false. The lesson here is to turn automatic redirection off. Fortunately, we do not need to visit http://ogame.org/ at all, because those cookies are not what logs us into the game.
+
+- When we send the kid, universe, username, and pass parameters to the login endpoint, we receive as a response an "OG_lastServer" cookie. We do not need this, but we will manually follow the redirection. The Location response header also contains the URL which encodes the cookie data as well as parameters for the second HTTP request, which will be a GET request.
+
+- In the second HTTP response, we receive 4 cookies: PHPSESSID, prsess_188304, login_188304, and language. We only need to hold on to PHPSESSID, prsess_188304, and login_188304. We then follow a redirect for a 3rd HTTP request and send these 3 cookies with our request. This should lead us to the overview screen, which we can then parse and extract out all the good fleet details. These cookies' data are also stored in the Location response header. Interestingly, the 4 cookies are set in a single Set-Cookie header. These cookies are automatically separated for us, but the job to parse the cookies and store them as HttpCookie objects falls on us (no thanks to HttpCookie.parse()'s buggy implementation).
+
+- The 3rd HTTP request is a GET request, which nets us our goal: the overview page.
--- src/com/wikaba/ogapp/agent/OgameAgent.java
+++ src/com/wikaba/ogapp/agent/OgameAgent.java
@@ -1,16 +1,32 @@
 package com.wikaba.ogapp.agent;
 
+
+import android.annotation.SuppressLint;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookieStore;
+import java.net.HttpCookie;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * This class represents 1 account on 1 universe. If the user is playing multiple accounts simultaneously,
@@ -21,6 +37,14 @@ import java.net.URLEncoder;
  */
 public class OgameAgent {
 	public static final String LOGIN_URL = "http://en.ogame.gameforge.com/main/login";
+//	public static final String LOGIN_URL = "http://localhost:8080/";
+	
+	private Map<String, HttpCookie> cookieStore;
+	
+	public OgameAgent() {
+//		CookieHandler handler = CookieHandler.getDefault();
+		cookieStore = new HashMap<String, HttpCookie>();
+	}
 	
 	/**
 	 * Submits user credentials to Ogame. Parses and returns data from HTTP response
@@ -32,67 +56,208 @@ public class OgameAgent {
 		final int timeoutMillis = 30 * 1000;
 		HttpURLConnection connection = null;
 		
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		
+		String uri = LOGIN_URL;
+		boolean isFirstRequest = true;
+		boolean locationToFollow = true;
+		
+		String parameters;
 		try {
-			connection = (HttpURLConnection)(new URL(LOGIN_URL)).openConnection();
-			connection.setRequestMethod("POST");
-			connection.setConnectTimeout(timeoutMillis);
-			connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-			connection.setRequestProperty("Accept-Encoding", "gzip, deflate");
-			connection.setRequestProperty("DNT", "1");
-			connection.setRequestProperty("Referer", "http://en.ogame.gameforge.com/");
-			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:32.0) Gecko/20100101 Firefox/32.0");
-			connection.setInstanceFollowRedirects(true);
-			connection.setDoOutput(true);
-			
-			Writer writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-			
-			String parameters = "kid=&universe=" + URLEncoder.encode(universe, "UTF-8") + "&login=" + URLEncoder.encode(username, "UTF-8") + "&pass=" + URLEncoder.encode(password, "UTF-8");
-			System.out.println(parameters);
-			writer.write(parameters);
-			writer.flush();
-			writer.close();
-			connection.connect();
-			
-			int response = connection.getResponseCode();
-			if(response == HttpURLConnection.HTTP_OK) {
-				System.out.println("Everything went okay!");
+			parameters = "kid=&uni=" + URLEncoder.encode(universe, "UTF-8") + "&login=" + URLEncoder.encode(username, "UTF-8") + "&pass=" + URLEncoder.encode(password, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			System.out.println("Error: " + e1 + '\n' + e1.getMessage());
+			e1.printStackTrace();
+			return null;
+		}
+		String length = Integer.toString(parameters.length());
+		
+		while(locationToFollow) {
+			locationToFollow = false;
+			try {
+				connection = (HttpURLConnection)(new URL(uri)).openConnection();
+				URI theUri = new URI(uri);
+				connection.setConnectTimeout(timeoutMillis);
+				connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+				//Set cookies.
+				boolean hasCookies = false;
+				boolean cookieAddedYet = false;
+				Collection<HttpCookie> cookies = cookieStore.values();
+				StringBuilder cookieHeaderBuilder = new StringBuilder();
+				Iterator<HttpCookie> cookieIter = cookies.iterator();
+				while(cookieIter.hasNext()) {
+					HttpCookie cookie = cookieIter.next();
+					String locationDomain = theUri.getAuthority();
+					String cookieDomain = cookie.getDomain();
+					String locationPath = theUri.getPath();
+					String cookiePath = cookie.getPath();
+					
+					/* add the cookie to the list to be sent only if the URI authority (the domain)
+					 * contains the cookie's domain.
+					 */
+					if(cookieDomain != null && !locationDomain.equals(cookieDomain) && cookieDomain.length() > 0 && cookieDomain.charAt(0) != '.') {
+						cookieDomain = '.' + cookieDomain;
+					}
+					if(locationDomain.contains(cookieDomain) && locationPath.contains(cookiePath)) {
+						if(cookieAddedYet == true) {
+							cookieHeaderBuilder.append(',');
+						}
+						cookieHeaderBuilder.append(cookie.getName())
+						.append('=')
+						.append(cookie.getValue());
+						cookieAddedYet = true;
+						hasCookies = true;
+					}
+				}
+				String cookieHeaderStr = cookieHeaderBuilder.toString();
+				if(hasCookies)
+					connection.setRequestProperty("Cookie", cookieHeaderStr);
 				
-				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				String line;
-				while((line = reader.readLine()) != null) {
-					buffer.append(line);
-					buffer.append('\n');
+				connection.setInstanceFollowRedirects(false);
+				
+				if(isFirstRequest) {
+					connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+					connection.setRequestProperty("Content-Length", length);
+					connection.setDoOutput(true);
+					connection.setRequestMethod("POST");
+					Writer writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+					System.out.println(parameters);
+					writer.write(parameters);
+					writer.flush();
+					writer.close();
+					isFirstRequest = false;
 				}
-				reader.close();
-			}
-			else {
-				System.err.println("Something went wrong!");
-				BufferedReader errReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-				String line;
-				while((line = errReader.readLine()) != null) {
-					buffer.append(line);
-					buffer.append('\n');
+				else {
+					connection.setRequestMethod("GET");
 				}
-				errReader.close();
+				
+				connection.connect();
+				
+				int response = connection.getResponseCode();
+				if(response == HttpURLConnection.HTTP_OK || (response >= 300 && response < 400)) {
+					System.out.println("Everything went okay!");
+					
+					Map<String, List<String>> responseHeaders = connection.getHeaderFields();
+					List<String> cookieHeaders = responseHeaders.get("Set-Cookie");
+					for(String cookieHeader : cookieHeaders) {
+						System.out.println(cookieHeader);
+						List<HttpCookie> cookiesList = parseCookies(cookieHeader, theUri.getAuthority(), theUri.getPath());
+						for(HttpCookie cookie : cookiesList) {
+							cookieStore.put(cookie.getName(), cookie);
+						}
+					}
+					
+					BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					String line;
+					while((line = reader.readLine()) != null) {
+						buffer.append(line);
+						buffer.append('\n');
+					}
+					reader.close();
+					
+					List<String> locationHeader = responseHeaders.get("Location");
+					if(locationHeader != null && locationHeader.size() > 0) {
+						uri = locationHeader.get(0);
+						locationToFollow = true;
+					}
+				}
+				else {
+					System.err.println("Something went wrong!");
+					BufferedReader errReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+					String line;
+					while((line = errReader.readLine()) != null) {
+						buffer.append(line);
+						buffer.append('\n');
+					}
+					errReader.close();
+				}
 			}
-		}
-		catch(MalformedURLException e) {
-			System.err.println("Something wrong happened! " + e + '\n' + e.getMessage());
-			e.printStackTrace();
-		}
-		catch(IOException e) {
-			System.err.println("Something wrong happened! " + e + '\n' + e.getMessage());
-			e.printStackTrace();
-		}
-		finally {
-			if(connection != null) {
-				connection.disconnect();
+			catch(MalformedURLException e) {
+				System.err.println("Something wrong happened! " + e + '\n' + e.getMessage());
+				e.printStackTrace();
+			}
+			catch(IOException e) {
+				System.err.println("Something wrong happened! " + e + '\n' + e.getMessage());
+				e.printStackTrace();
+			}
+			catch (URISyntaxException e) {
+				System.err.println("URI error: " + e + '\n' + e.getMessage());
+				e.printStackTrace();
+			}
+			catch(Exception e) {
+				System.err.println("What went wrong: " + e + '\n' + e.getMessage());
+				e.printStackTrace();
+			}
+			finally {
+				if(connection != null) {
+					connection.disconnect();
+				}
 			}
 		}
 			
 		return buffer;
+	}
+	
+	/**
+	 * We have to implement our own cookie parser because Java's HttpCookie.parse() method throws an Exception when
+	 * it should not throw that Exception.
+	 * @param cookieStr - The header value from one Set-Cookie header field. May contain multiple multiple cookies.
+	 * @return A list of HttpCookie objects representing the cookies in cookieStr
+	 */
+	private List<HttpCookie> parseCookies(String cookieStr, String defaultDomain, String defaultPath) {
+		HttpCookie cookie = null;
+		String[] cookieTokens = cookieStr.split(";");
+		
+		String name = null;
+		String value = null;
+		String expiration = null;
+		String path = null;
+		String domain = null;
+		
+		for(String tok : cookieTokens) {
+			String[] tokpieces = tok.split("=");
+			if(tokpieces.length == 2) {
+				tokpieces[0] = tokpieces[0].trim();
+				tokpieces[1] = tokpieces[1].trim();
+				
+				if(tokpieces[0].equalsIgnoreCase("expires")) {
+					expiration = tokpieces[1];
+				}
+				else if(tokpieces[0].equalsIgnoreCase("path")) {
+					path = tokpieces[1];
+				}
+				else if(tokpieces[0].equalsIgnoreCase("domain")) {
+					domain = tokpieces[1];
+				}
+				else { //name of the cookie
+					name = tokpieces[0];
+					value = tokpieces[1];
+				}
+			}
+		}
+		
+		if(name == null)
+			return null;
+		
+		cookie = new HttpCookie(name, value);
+		cookie.setVersion(0);
+		if(expiration != null) {
+			//set expiration
+		}
+		if(path != null) {
+			cookie.setPath(path);
+		}
+		else {
+			cookie.setPath(defaultPath);
+		}
+		if(domain != null) {
+			cookie.setDomain(domain);
+		}
+		else {
+			cookie.setDomain(defaultDomain);
+		}
+		ArrayList<HttpCookie> cookieList = new ArrayList<HttpCookie>();
+		cookieList.add(cookie);
+		return cookieList;
 	}
 }
