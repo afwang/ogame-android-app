10cd66d95da0e157f1015c9fdd3a5558d9d94d3f
Alexander Wang <afwang@lolcomp.wikaba.com>
2014-11-29 11:58:45 -0800

    Added license and copyright headers to all files

--- src/com/wikaba/ogapp/AgentService.java
+++ src/com/wikaba/ogapp/AgentService.java
@@ -1,3 +1,23 @@
+/*
+
+    Copyright 2014 Alexander Wang
+    
+    This file is part of Ogame on Android.
+
+    Ogame on Android is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ogame on Android is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ogame on Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.wikaba.ogapp;
 
 import java.util.ArrayList;
@@ -20,7 +40,7 @@ public class AgentService extends Service {
 	
 	private IBinder mBinder;
 	private LongSparseArray<OgameAgent> ogameSessions;
-	private DatabaseManager dbman;
+	private volatile DatabaseManager dbman;
 	
 	public AgentService() {
 	}
@@ -35,11 +55,16 @@ public class AgentService extends Service {
 			ogameSessions = new LongSparseArray<OgameAgent>();
 		}
 		
+		onRebind(intent);
+		
+		return mBinder;
+	}
+	
+	@Override
+	public void onRebind(Intent intent) {
 		if(dbman == null) {
 			dbman = new DatabaseManager(this);
 		}
-		
-		return mBinder;
 	}
 	
 	@Override
@@ -49,7 +74,7 @@ public class AgentService extends Service {
 			dbman = null;
 		}
 		
-		return false;
+		return true;
 	}
 	
 	/**
